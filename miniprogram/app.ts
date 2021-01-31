39627ce94014df058129d4b90b9bb2cba4f8bdc3
GaGaChina <15020055@qq.com>
2021-01-31 17:27:54 +0800

    change

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,10 +1,9 @@
-import { $g } from "./frame/speed.do";
-import { WXSoterAuth } from "./frame/wx/wx.soter.auth";
+import { $g } from "./frame/speed.do"
+import { WXSoterAuth } from "./frame/wx/wx.soter.auth"
 import { WXUser } from "./frame/wx/wx.user"
+import { WXFile } from "./frame/wx/wx.file"
 import { WXSize } from "./frame/wx/wx.resize"
-import { GDataLib } from "./lib/g-data-lib/g.data.lib";
-import { DBLib } from "./lib/g-data-lib/db.lib";
-import { WXFile } from "./frame/wx/wx.file";
+import { DBLib } from "./lib/g-data-lib/db.lib"
 
 App<IAppOption>({
     globalData: {
@@ -38,6 +37,7 @@ App<IAppOption>({
         // 获取 Storage 的数据来设置
         const dbLib:DBLib = this.globalData.dbLib
         dbLib.storageSetThis()
+        dbLib.checkFile()
         dbLib.fileSizeRun()
         // 版本升级需要维护本地缓存数据
         $g.log('版本检查 : ' + this.globalData.app.ver + ' → ' + $g.s.getS('app.ver'))
@@ -46,26 +46,26 @@ App<IAppOption>({
             // 获取需要保存的数据
             $g.s.copySG('user', '*,!id,!wx,!wxEncryptedData,!wxSignature,!wxIv,!wxCode,!promoter');
             //清理服务器数据
-            $g.s.storageClear();
+            $g.s.storageClear()
             //创建保存数据,进行保存
-            this.globalData.dbLib.storageSaveThis()
-            $g.s.copyGS('app', 'ver');
-            $g.s.copyGS('user', '*');
-            $g.s.copyGS('userWX', '*');
-            $g.s.copyGS('dbInfo', '*');
+            dbLib.storageSaveThis()
+            $g.s.copyGS('app', 'ver')
+            $g.s.copyGS('user', '*')
+            $g.s.copyGS('userWX', '*')
+            $g.s.copyGS('dbInfo', '*')
         }
         // 读取 app 中 除了 appVer 的内容
-        $g.s.copyGS('app', '*,!appVer');
+        $g.s.copyGS('app', '*,!appVer')
         // 获取缓存的用户数据
-        $g.s.copySG('user', '*');
-        $g.s.copySG('userWX', '*');
+        $g.s.copySG('user', '*')
+        $g.s.copySG('userWX', '*')
         // 检查数据
         $g.log('[App.globalData]', this.globalData)
         $g.log('[App.Storage]', $g.s.storageAll())
         $g.s.storageInfo()
         WXFile.checkFileList('', true)
         // 登录
-        WXUser.wxCode();
+        WXUser.wxCode()
         // 获取用户信息
         WXUser.wxGetSetting()
         // 获取生物认证状况
